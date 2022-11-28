406815b769659d163259a060f03d66ebe0957057
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2022-11-28 22:32:04 +0800

    UI: Further code cleanup and refactoring

---
+++ src/components/UI/SVG.vue
@@ -0,0 +1,23 @@
+<template>
+    <svg class="icon " :class="this.size" xmlns="http://www.w3.org/2000/svg" :viewBox="this.viewBox">
+        <path :d="this.d" />
+    </svg>
+</template>
+<script>
+export default {
+    props: ["viewBox", "d", "size"],
+}
+</script>
+<style>
+svg.icon.sm {
+    width: 16px;
+    fill: currentColor;
+    margin-bottom: 0.25rem;
+}
+
+svg.icon.lg {
+    width: 20px;
+    fill: currentColor;
+    margin-bottom: 0.5rem;
+}
+</style>
